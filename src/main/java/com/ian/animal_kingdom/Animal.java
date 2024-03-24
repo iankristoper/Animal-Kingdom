fea349cead8ebd0253597a0b220b05d19cfb14aa
iankristoper <iankristoper26@gmail.com>
2024-03-25 06:10:44 +0800

    UPDATE: base class features

---
+++ src/main/java/com/ian/animal_kingdom/Animal.java
@@ -0,0 +1,46 @@
+package com.ian.animal_kingdom;
+
+/**
+ *
+ * @author Ian Tagano
+ */
+
+public abstract class Animal 
+{
+    //private data
+    private String name;
+    private int age;
+    private String habitat;
+    
+    //constructor
+    public Animal(String name, int age, String habitat)
+    {
+        this.name = name;
+        this.age = age;
+        this.habitat = habitat;
+    }
+    
+    
+    //getters 
+    public String getName()
+    {
+        return name;
+    }
+    
+    //getters
+    public int getAge()
+    {
+        return age;
+    }
+    
+    //getters
+    public String habitat()
+    {
+        return habitat;
+    }
+    
+    
+    //methods 
+    public abstract void makeSound();
+    
+}
