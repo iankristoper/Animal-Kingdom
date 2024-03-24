acefdc7a57a4258657d208acff0cbe7aee32a880
iankristoper <iankristoper26@gmail.com>
2024-03-25 06:04:18 +0800

    UPDATE: main class features

---
+++ src/main/java/com/ian/animal_kingdom/Animal_Kingdom.java
@@ -0,0 +1,15 @@
+package com.ian.animal_kingdom;
+
+/**
+ *
+ * @author Ian Tagano
+ */
+
+public class Animal_Kingdom 
+{
+
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello World!");
+    }
+}
