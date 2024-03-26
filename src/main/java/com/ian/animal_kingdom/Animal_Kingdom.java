303580f2ea8ef350a7e420b744a73e04ffa37608
iankristoper <iankristoper26@gmail.com>
2024-03-26 11:01:35 +0800

    UPDATE: main class features

--- src/main/java/com/ian/animal_kingdom/Animal_Kingdom.java
+++ src/main/java/com/ian/animal_kingdom/Animal_Kingdom.java
@@ -10,6 +10,29 @@ public class Animal_Kingdom
 
     public static void main(String[] args) 
     {
-        System.out.println("Hello World!");
+        Mammal lion = new Mammal("lions", 12, "savannah", true);
+        Reptile snake = new Reptile("snakes", 3, "rainforest", true);
+        Bird pigeon = new Bird("pigeon", 1, "mountains", true);
+        
+        System.out.println("Mammal");
+        System.out.println("Name: " + lion.getName());
+        System.out.println("Age: " + lion.getAge());
+        System.out.println("Habitat: " + lion.getHabitat());
+        System.out.println("Warm Blooded: " + lion.getBloodStatus());
+        lion.makeSound();
+        
+        System.out.println("\nBird");
+        System.out.println("Name: " + pigeon.getName());
+        System.out.println("Age: " + pigeon.getAge());
+        System.out.println("Habitat: " + pigeon.getHabitat());
+        System.out.println("Can fly: " + pigeon.isCanFly());
+        pigeon.makeSound();
+        
+        System.out.println("\nReptiles");
+        System.out.println("Name: " + snake.getName());
+        System.out.println("Age: " + snake.getAge());
+        System.out.println("Habitat: " + snake.getHabitat());
+        System.out.println("Warm Blooded: " + snake.getBloodStatus());
+        snake.makeSound();
     }
 }
